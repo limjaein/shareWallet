70b0b288cf70365bc3c240d2a76cab8f35a2a769
limjaein <dlawodls96@naver.com>
2023-05-15 15:45:39 +0900

    AppConfig 추가

--- src/main/java/com/pjt/sharewallet/AppConfig.java
+++ src/main/java/com/pjt/sharewallet/AppConfig.java
@@ -1,2 +1,16 @@
-package com.pjt.sharewallet;public class AppConfig {
+package com.pjt.sharewallet;
+
+import com.pjt.sharewallet.member.repository.MemoryMemberRepository;
+import com.pjt.sharewallet.member.service.MemberService;
+import com.pjt.sharewallet.member.service.MemberServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
 }
